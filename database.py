752358ce6afb02a22be693a338b063d063188790
danielnv051 <danielnv0910@gmail.com>
2024-02-10 02:35:58 +0330

    complete assignment 22

--- database.py
+++ database.py
@@ -6,20 +6,45 @@ class Database:
         self.con = sqlite3.connect("dolist.db")
         self.cursor = self.con.cursor()
 
-    def get_tasks(self):
-        query = "SELECT * FROM tasks"
-        result = self.cursor.execute(query)
-        tasks = result.fetchall()
+    def get_tasks(self, id):
+        if id == -1:
+            query = "SELECT * FROM tasks ORDER BY is_done,Priority DESC"
+            result = self.cursor.execute(query)
+            tasks = result.fetchall()
+        else:
+            idd = id[1:]
+            query = f"SELECT * FROM tasks WHERE id = {idd}"
+            result = self.cursor.execute(query)
+            tasks = result.fetchall()
         return tasks
 
-    def add_new_task(self, new_title, new_description):
+    def add_new_task(self, new_title, new_description, time, priority):
         try:
-            query = f"INSERT INTO tasks (title,desc) VALUES('{new_title}', '{new_description}')"
+            query = f"INSERT INTO tasks (title,desc,time,Priority) VALUES('{new_title}', '{new_description}','{time}', '{priority}')"
             self.cursor.execute(query)
             self.con.commit()
             return True
         except:
             return False
 
+    def task_done(self, id):
+        ids = id[1:]
+        query = f"SELECT * FROM tasks WHERE id = {ids}"
+        result = self.cursor.execute(query)
+        res = result.fetchall()
+        if res[0][3] == 1:
+            query = f"UPDATE tasks SET is_done = 0 WHERE id = {ids}"
+        else:
+            query = f"UPDATE tasks SET is_done = 1 WHERE id = {ids}"
 
-    def task_done(self, id): ...
+        self.cursor.execute(query)
+        self.con.commit()
+        return True
+
+    def task_delete(self, id):
+        ids = id[1:]
+        query = f"DELETE FROM tasks WHERE id = {ids}"
+        result = self.cursor.execute(query)
+        self.cursor.execute(query)
+        self.con.commit()
+        return True
--- main_window.py
+++ main_window.py
@@ -15,55 +15,49 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QTextEdit,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QPushButton,
+    QSizePolicy, QTabWidget, QWidget)
+import bg_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(396, 250)
+        MainWindow.resize(796, 491)
+        font = QFont()
+        font.setFamilies([u"Yekan Bakh FaNum"])
+        MainWindow.setFont(font)
+        MainWindow.setContextMenuPolicy(Qt.DefaultContextMenu)
+        MainWindow.setStyleSheet(u"")
+        MainWindow.setDocumentMode(True)
+        MainWindow.setTabShape(QTabWidget.Rounded)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.gridLayout = QHBoxLayout()
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.tb_new_task = QLineEdit(self.centralwidget)
-        self.tb_new_task.setObjectName(u"tb_new_task")
-
-        self.gridLayout.addWidget(self.tb_new_task)
-
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.add_new_task_btn = QPushButton(self.centralwidget)
+        self.add_new_task_btn.setObjectName(u"add_new_task_btn")
+        self.add_new_task_btn.setGeometry(QRect(360, 420, 81, 61))
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy)
-        self.pushButton.setMinimumSize(QSize(0, 30))
-        self.pushButton.setMaximumSize(QSize(16777215, 16777215))
-        font = QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        self.pushButton.setFont(font)
-
-        self.gridLayout.addWidget(self.pushButton)
-
-
-        self.verticalLayout.addLayout(self.gridLayout)
-
-        self.tb_new_desc = QTextEdit(self.centralwidget)
-        self.tb_new_desc.setObjectName(u"tb_new_desc")
-
-        self.verticalLayout.addWidget(self.tb_new_desc)
-
-        self.gl_tasks = QGridLayout()
+        sizePolicy.setHeightForWidth(self.add_new_task_btn.sizePolicy().hasHeightForWidth())
+        self.add_new_task_btn.setSizePolicy(sizePolicy)
+        self.add_new_task_btn.setMinimumSize(QSize(0, 30))
+        self.add_new_task_btn.setMaximumSize(QSize(16777215, 16777215))
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI"])
+        font1.setPointSize(20)
+        font1.setBold(True)
+        self.add_new_task_btn.setFont(font1)
+        self.add_new_task_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.add_new_task_btn.setStyleSheet(u"border:none;\n"
+"image: url(:/btn/plus.png);\n"
+"")
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 25, 781, 391))
+        self.gl_tasks = QGridLayout(self.layoutWidget)
         self.gl_tasks.setObjectName(u"gl_tasks")
-
-        self.verticalLayout.addLayout(self.gl_tasks)
-
+        self.gl_tasks.setContentsMargins(0, 0, 0, 0)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -73,6 +67,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.add_new_task_btn.setText("")
     # retranslateUi
 
